828bb5bc925bb353afaa97c781caf1703516f1f7
AshtonF04 <ashtonfranklin772@gmail.com>
2024-08-11 22:33:27 -0400

    Finished login page UI

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,28 +1,18 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { initializeApp } from '@firebase/app';
+import { Link } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from '@firebase/auth';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD_FEcmTpO9deMVra57bkfuvgTr96VdmpU",
-  authDomain: "edutrack-607f2.firebaseapp.com",
-  projectId: "edutrack-607f2",
-  storageBucket: "edutrack-607f2.appspot.com",
-  messagingSenderId: "301111660851",
-  appId: "1:301111660851:web:e56ea2289126f834152e46",
-  measurementId: "G-D0PEPV2WDW"
-};
-
-const firebaseApp = initializeApp(firebaseConfig)
 
 const index = () => {
   return (
-    <View>
-        <SafeAreaView>
-            <Text>Login</Text>
-        </SafeAreaView>
+    <View className="bg-[#F1F1F1] h-screen flex justify-center">
+        <View className="mt-4 flex items-center">
+            <Text className="text-center text-xl font-bold">Login</Text>
+            <TextInput className="bg-white w-72 h-7 rounded-md mt-4 p-1" placeholder='Email'></TextInput>
+            <TextInput className="bg-white w-72 h-7 rounded-md mt-4 p-1" placeholder='Password'></TextInput>
+            <Link href="/dashboard" className='mt-10 text-blue-400'>No account? Signup instead.</Link>
+        </View>
     </View>
   )
 }
